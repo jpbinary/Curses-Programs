2ecaff1e910ee052c20ebb661df75fd6ef0a2975
jpbinary <jp@iskyy.com>
2015-10-17 20:27:38 -0500

    says hello

--- my_curses_example.py
+++ my_curses_example.py
@@ -1,15 +1,38 @@
 import curses
+import time
 
-stdscr = curses.initscr() # initialize curses. stdscr is a window object that covers the entire screen
-curses.noecho() # turn of auto echoing of keys
-curses.cbreak() # react to key presses instantly, dont require Enter key
-stdscr.keypad(1) # have curses process special keys
+# many comments from documentation at: https://docs.python.org/2/howto/curses.html#curses-howto
 
-begin_x = 20; begin_y = 7
-height = 5; width = 40
-win = curses.newwin(height, width, begin_y, begin_x)
+'''
+The following is automated by using curses.wrapper:
+    stdscr = curses.initscr() # initialize curses. stdscr is a window object that covers the entire screen
+    curses.noecho() # turn of auto echoing of keys
+    curses.cbreak() # react to key presses instantly, dont require Enter key
+    stdscr.keypad(1) # have curses process special keys
 
-# terminate curses gracefully
-curses.nocbreak(); stdscr.keypad(0); curses.echo()
-curses.endwin()
+    begin_x = 20; begin_y = 7 # coordinates are always passed in the order y,x, and the top-left corner of a window is coordinate (0,0)
+    height = 5; width = 40
 
+    # terminate curses gracefully
+    curses.nocbreak(); stdscr.keypad(0); curses.echo()
+    curses.endwin()
+    stdscr.refresh() # must manually refresh screen
+'''
+
+
+class ScreenApp(object):
+
+    def __init__(self, stdscreen):
+        self.screen = stdscreen
+        curses.curs_set(0)
+
+        self.screen.addstr("1.) Say Hello")
+        self.screen.refresh()
+        self.screen.addstr("2.) Say Goodbye")
+        self.screen.refresh()
+        time.sleep(5.5)
+
+if __name__ == '__main__':
+    curses.wrapper(ScreenApp)
+    '''curses.wrapper() does the initializations described above, and also initializes colors if color support is
+    present. It then runs your provided callable and finally deinitializes appropriately.'''
